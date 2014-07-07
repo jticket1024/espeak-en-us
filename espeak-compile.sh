979cdf95e9afb29971ffd5196f0b5bedcc424f30
kendell clark <coffeekingms@gmail.com>
2014-07-07 18:57:03 -0500

    added an improved version of the espeak compilation script.

--- espeak-compile.sh
+++ espeak-compile.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#make sure this script is not started as root cause it can mess up read/write perms on the directory
+if [[ $(whoami) == "root" ]] ; then
+    echo "this script should not be initiated as root. It will copy files using sudo."
+exit 0
+fi
+
 #check for updates.
 git pull
 
@@ -16,30 +22,18 @@ fi
 
 #need root permission to proceed
 echo "Copying files:"
-if [[ $(whoami) != "root" ]] ; then
 #copy files to espeak directory
 sudo cp ./en_* /usr/share/espeak-data/
 #change to espeak directory and compile data
 cd /usr/share/espeak-data/
 sudo espeak --compile=en-us
 echo "files updated."
-read -n 1 -p "Restart espeakup using systemctl? " restart
-if [ "${restart^}" == "Y" ] ; then
-serviceName="$(systemctl --no-pager | grep espeakup | head -n 1 | cut -d " " -f1)"
-sudo systemctl restart $serviceName
-fi
-else
-#copy files to espeak directory
-cp ./en_* /usr/share/espeak-data/
-#change to espeak directory and compile data
-cd /usr/share/espeak-data/
-espeak --compile=en-us
-echo "files updated."
-read -n 1 -p "Restart espeakup using systemctl? " restart
-if [ "${restart^}" == "Y" ] ; then
-serviceName="$(systemctl --no-pager | grep espeakup | head -n 1 | cut -d " " -f1)"
-systemctl restart $serviceName
-fi
+serviceName="$(systemctl --no-pager --all | grep -E espeakup.*service | cut -d " " -f3)"
+if [ -n "$serviceName" ] ; then
+    read -n 1 -p "Restart $serviceName? " restart
+    if [ "${restart^}" == "Y" ] ; then
+        sudo systemctl restart $serviceName
+    fi
 fi
 echo
 exit 0
